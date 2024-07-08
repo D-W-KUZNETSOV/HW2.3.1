116a9e044e390004ceb273212a15365cf6c1125b
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-07-08 20:02:02 +0300

    Calculator

---
+++ src/main/java/pro/sky/skyprocakculator/SkyproCakculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocakculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCakculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCakculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprocakculator/SkyproCakculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocakculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCakculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
